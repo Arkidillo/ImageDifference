769ada47df4b8dac6af140fe85d1625c767e1c39
Devin Kopp <devinkopp@gwmail.gwu.edu>
2017-11-21 13:39:25 -0500

    Make sure image is accessible after 'with'

--- image.py
+++ image.py
@@ -2,5 +2,9 @@ from PIL import Image, ImageFilter
 from urllib.request import urlopen
 import io
 
+# Download the image
 with urlopen("http://128.164.158.1/jpg/image.jpg") as conn:
     img = Image.open(conn)
+
+
+print(img)
